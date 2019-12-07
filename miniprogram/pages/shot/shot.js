f7b26772e58b97d1a55f5c4ed3dc76f4855c0ebe
JudyHU <992617751@qq.com>
2019-12-07 14:48:56 +0800

    call camara

--- miniprogram/pages/shot/shot.js
+++ miniprogram/pages/shot/shot.js
@@ -62,5 +62,21 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  // 拍摄或从相册选取上传
+  uploadPhoto(e) {
+    var that = this;
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths;
+          console.log('本地图片的路径:', tempFilePaths)
+        upload(that, tempFilePaths)
+      }
+    })
+  },
+  
 })
