4218e61158c47d31c93bcf8a7bf8b3bf691ee2f2
JudyHU <992617751@qq.com>
2019-12-08 22:21:50 +0800

    finish camera

--- miniprogram/pages/shot/shot.js
+++ miniprogram/pages/shot/shot.js
@@ -5,9 +5,63 @@ Page({
    * 页面的初始数据
    */
   data: {
+    carWin_img_hidden: true, //展示照片的view是否隐藏
+    carWin_img: '' ,//存放照片路径的
+    actionSheetHidden:true,
+    actionSheetItems:[
+      {bindtap:'camara',txt:'相机'},
+      {bindtap:'album',txt:'从相册中选取' },
+      
+    ],
+   
+    
+  },
+  clickpics: function (way) {
+    var that = this;
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: way, // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 无论用户是从相册选择还是直接用相机拍摄，路径都是在这里面
+        var filePath = res.tempFilePaths[0];
+        //将刚才选的照片/拍的 放到下面view视图中
+        that.setData({
+          carWin_img: filePath, //把照片路径存到变量中，
+          carWin_img_hidden: false //让展示照片的view显示
+        });
+      }
+    })
+  },
+  actionSheetTap: function () {
+
+    this.setData({
+
+      actionSheetHidden: !this.data.actionSheetHidden
+
+    })
 
   },
-
+  actionSheetbindchange: function () {
+    this.setData({
+      actionSheetHidden: !this.data.actionSheetHidden
+    })
+  },
+  bindcamara: function () {
+    var way = "camera";
+    this.setData({
+      actionSheetHidden: !this.data.actionSheetHidden
+    });
+    this.clickpics(way);
+  },
+  bindalbum: function () {
+    this.setData({
+      way:"album",
+      actionSheetHidden: !this.data.actionSheetHidden
+    });
+    this.clickpics(way);
+  },
+  
   /**
    * 生命周期函数--监听页面加载
    */
@@ -19,7 +73,10 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    this.setData({
+      carWin_img_hidden: true,
+      carWin_img: ''
+    });
   },
 
   /**
@@ -63,20 +120,9 @@ Page({
   onShareAppMessage: function () {
 
   },
+  
   // 拍摄或从相册选取上传
-  uploadPhoto(e) {
-    var that = this;
-    wx.chooseImage({
-      count: 1, // 默认9
-      sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
-      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-      success: function (res) {
-        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-        var tempFilePaths = res.tempFilePaths;
-          console.log('本地图片的路径:', tempFilePaths)
-        upload(that, tempFilePaths)
-      }
-    })
-  },
+  
+  
   
 })
