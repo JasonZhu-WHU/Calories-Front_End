9e07861b3941bb07a73d5bdfa60c1ff148637ecb
Drbuilders <928910195@qq.com>
2019-12-13 21:07:23 +0800

    modify my

--- miniprogram/pages/my/my.js
+++ miniprogram/pages/my/my.js
@@ -1,25 +1,88 @@
 // pages/my/my.js
+const app = getApp()
+
 Page({
   /**
   * 页面的初始数据
   */
   data: {
     index: 0,
-    score: 0,
+    bmi: 0,
+    inputHeight:0,
+    inputWeight:0,
     height: 0,
     weight: 0,
   },
+  onLoad: function () {
+    var that = this
+    wx.request({
+      url: 'https://csquare.wang/user',
+      method: 'GET',
+      data: {
+        openId: app.globalData.openid,        //需传入用户openid
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        that.setData({
+          inputHeight: res.data.height,
+          inputweight: res.data.weight,
+          bmi: res.data.bmi,
+        })
+      }
+    })
+  },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  onShow: function () {
+    var that = this
+    wx.request({
+      url: 'https://csquare.wang/user',
+      method: 'GET',
+      data: {
+        openId: app.globalData.openid,        //需传入用户openid
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        that.setData({
+          inputHeight: res.data.height,
+          inputweight: res.data.weight,
+          bmi: res.data.bmi,
+        })
+      }
+    })
+  },
+
+  onHide: function () {
+    var that = this;
+    wx.request({
+      url: 'https://csquare.wang/user',
+      method: 'GET',
+      data: {
+        openId: app.globalData.openid,        //需传入用户openid
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        that.setData({
+          inputHeight: res.data.height,
+          inputweight: res.data.weight,
+          bmi: res.data.bmi,
+        })
+      }
+    })
+  },
+  
   bindPickerChange: function (e) {
     this.setData({
       index: e.detail.value
     })
   },
 
-  bindKeyHightInput: function (e) {
+  bindKeyHightInput: function (e) {    
     this.setData({
       height: e.detail.value
     })
@@ -46,17 +109,33 @@ Page({
       return false;
     }
     this.calculate();
-    this.weightStandardCalculate();
-    this.physicalConditionCalculate();
+
+    wx.request({
+      url: 'https://csquare.wang/user',
+      method: 'POST',
+      data: {
+        openId: app.globalData.openId,        //需传入用户openid
+        reqParam: {
+          height: this.data.height,
+          weight: this.data.weight,
+          bmi: this.data.bmi,
+          weight: this.data.dishWeigh,
+        }
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+    })
+
   },
 
-  //计算IBM值
+  //计算BMI值
   calculate: function () {
-    let score = 0;
+    let bmi = 0;
     let height = this.data.height / 100;
-    score = (this.data.weight / (height * height)).toFixed(1);
+    bmi = (this.data.weight / (height * height)).toFixed(1);
     this.setData({
-      score: score
+      bmi: bmi
     })
   },
 
@@ -64,22 +143,26 @@ Page({
   * 生命周期函数--监听页面初次渲染完成
   */
   onReady: function () {
-
+    var that = this
+    wx.request({
+      url: 'https://csquare.wang/user',
+      method: 'GET',
+      data: {
+        openId: app.globalData.openid,        //需传入用户openid
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        that.setData({
+          inputHeight: res.data.height,
+          inputweight: res.data.weight,
+          bmi: res.data.bmi,
+        })
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
 
   /**
    * 生命周期函数--监听页面卸载
